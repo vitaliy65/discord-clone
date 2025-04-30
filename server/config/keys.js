d35cd2f6810be40d4f24b595ba4cfe3edc9a7417
Vitaliy <vitaliy60606@gmail.com>
2025-04-30 16:02:59 +0300

    added packages to server

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+export const mongoURI =
+  "mongodb+srv://{user}:{password}@{nameDB}.lr4zm.mongodb.net/?retryWrites=true&w=majority&appName={nameDB}";
+export const secretOrKey = "oiwhrjapoiasjf89p32y22q9222h2g9w34-5nugh903-qb2hug";
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import { config } from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import { mongoURI } from "./config/keys.js";
 
 // Load environment variables
 config();
@@ -13,11 +17,23 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+// DB Config
+const db = mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 // Create HTTP server
 const httpServer = createServer(app);
 
-// Initialize Socket.IO
+// Initialize Socket.IO ===================================================================
 const io = new Server(httpServer, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -33,6 +49,7 @@ io.on("connection", (socket) => {
     console.log("Client disconnected:", socket.id);
   });
 });
+// ========================================================================================
 
 // Basic route
 app.get("/", (req, res) => {
@@ -42,9 +59,7 @@ app.get("/", (req, res) => {
 // Start server
 httpServer.listen(PORT, () => {
   console.log(`\nServer running on port: ${PORT}`);
-  console.log(`Socket.IO server running at http://localhost:${PORT}`);
-  console.log(
-    `Client URL: ${process.env.CLIENT_URL || "http://localhost:5173"}`
-  );
+  console.log(`Socket.IO server running at ${process.env.CLIENT_URL}:${PORT}`);
+  console.log(`Client URL: ${process.env.CLIENT_URL}`);
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
 });
