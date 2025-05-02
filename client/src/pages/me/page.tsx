e8abab6a81b286f68ebe7769e24b59497477a5a5
Vitaliy <vitaliy60606@gmail.com>
2025-05-02 18:35:37 +0300

    added redux store

--- client/src/pages/me/page.tsx
+++ client/src/pages/me/page.tsx
@@ -1,3 +1,15 @@
+import Channels from "@/components/userMainPage/channels";
+import Friends from "@/components/userMainPage/friends";
+import Chat from "@/components/userMainPage/chat";
+
+import "@/styles/pages/me/userMainPage.css";
+
 export default function Me() {
-  return <div>Me</div>;
+  return (
+    <div className="user-main-page-container">
+      <Channels></Channels>
+      <Friends></Friends>
+      <Chat></Chat>
+    </div>
+  );
 }
